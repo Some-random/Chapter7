0550d554082f2cf46e495ce8579fd233510c9c59
Bob_JIANG <jdw19930725@gmail.com>
2015-08-14 16:41:54 +0800

    Finished Problem2, I left my solution in comments under main function

---
+++ src/Problem2.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Bob_JIANG on 8/14/2015.
+ */
+public class Problem2 {
+    //I left my solution in comments under main function
+    public static void main(String[] args) {
+        //For the version with 3 ants, it's obvious there are only two ways in which no ants meet each other. It works
+        // for polygon too. So the odds of collision is 1 - 2 / 2^n
+    }
+}
