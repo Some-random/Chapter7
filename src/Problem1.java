5d2b9b45af0bac486613244ddfb5dcc94bb076d7
Bob_JIANG <jdw19930725@gmail.com>
2015-08-14 15:42:23 +0800

    Finished Problem1, I left my solution in comments under main function

---
+++ src/Problem1.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Bob_JIANG on 8/14/2015.
+ */
+public class Problem1 {
+    //I left my solution in comments under main function
+    public static void main(String[] args) {
+        //The odds for first approach is p, the odds for second approach is (2, 3) * p * p = 3 * p * p
+        //So when p is greater than 1 / 3, go for second approach; else, go for first
+    }
+}
